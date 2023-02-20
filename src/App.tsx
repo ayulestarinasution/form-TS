49ba1cbab540bfefe7169d729a0bad2c68acd58b
Ayu Lestari <ayulestarinasution848@gmail.com>
2023-02-20 19:28:57 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container py-4">
+     <h1 className='mb-0'>React Form</h1>
+     <p>Learn how to use form in react</p>
+     <button className='btn btn-primary'>
+      <em className="fas fa-plus"></em> Add</button>
     </div>
   );
 }
